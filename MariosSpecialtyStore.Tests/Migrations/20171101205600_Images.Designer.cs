00d4b3a82b0dfe693a737f0b9f95470d26cc4776
Jesse Bryan <jesse.bryan22@gmail.com>
2017-11-01 14:01:27 -0700

    fix some styling issues

--- MariosSpecialtyStore.Tests/Migrations/20171101205600_Images.Designer.cs
+++ MariosSpecialtyStore.Tests/Migrations/20171101205600_Images.Designer.cs
@@ -8,9 +8,10 @@ using MariosSpecialtyStore.Models;
 namespace MariosSpecialtyStore.Tests.Migrations
 {
     [DbContext(typeof(TestDbContext))]
-    partial class TestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171101205600_Images")]
+    partial class Images
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");
